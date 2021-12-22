258ffc057ce68e9716e1ccd099de1bb8662aaddd
earl-stephens <stephensearl@hotmail.com>
2021-12-21 18:11:29 -0700

    Completed coding challenge

--- src/application/App.java
+++ src/application/App.java
@@ -1,25 +1,52 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
-	/*Create a program that asks a user to enter a command.
+	/*
+	 * Create a program that asks a user to enter a command.
 	 * 
-	 * If they hit the return keey without entering anything, ask them again 
-	 * to enter a command.
+	 * If they hit the return key without entering anything, ask them again to enter
+	 * a command.
 	 * 
 	 * If they enter 'start' or 'START', regardless of capitalization, print
 	 * 'Machine starting...'
 	 * 
-	 * If they enter 'stop', regardless of capitalization, print
-	 * 'Machine stopping...'
+	 * If they enter 'stop', regardless of capitalization, print 'Machine
+	 * stopping...'
 	 * 
 	 * If they enter 'quit', print 'Exiting...' and quit the program.
 	 * 
 	 * Otherwise print 'Unrecognized'.
 	 * 
-	 * Ask the to enter a command repeatedly until they stop the program by
-	 * entering 'quit'.
+	 * Ask the to enter a command repeatedly until they stop the program by entering
+	 * 'quit'.
 	 */
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		String userInput = null;
+
+		do {
+			System.out.print("Enter a command: ");
+			userInput = scanner.nextLine();
+
+			if (userInput.equalsIgnoreCase("start")) {
+				System.out.println("Machine starting...");
+			} else if (userInput.equalsIgnoreCase("stop")) {
+				System.out.println("Machine stopping...");
+			} else if (userInput.equalsIgnoreCase("quit")) {
+				break;
+			} else if (userInput.length() == 0) {
+			}
+			else {
+				System.out.println("Unrecognized");
+			}
+		} while (true);
+
+		scanner.close();
+		System.out.println("Exiting...");
 		
 	}
 }
